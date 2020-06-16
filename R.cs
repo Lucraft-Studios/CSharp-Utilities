b3bd587a45b51d8fced8be35540e1ddae2c27bb7
Luca Lewin <luca.lewin23@gmail.com>
2020-06-16 13:19:02 +0200

    created R.cs

---
+++ R.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+
+namespace Lucraft.Utilities
+{
+    public class R
+    {
+
+        public static Strings Strings { get; }
+        public static Colors Colors { get; }
+
+        static R()
+        {
+            Strings = null;
+            Colors = null;
+        }
+
+        private R() { }
+
+    }
+
+    public class Strings : Resource<string>
+    {
+
+    }
+
+    public class Colors : Resource<Color>
+    {
+
+    }
+
+    public abstract class Resource<T>
+    {
+
+    }
+
+    class Tester
+    {
+        void Test()
+        {
+            
+        }
+    }
+
+}
