738281b928762f60c63c906daf5d37c04650674d
Luca Lewin <luca.lewin23@gmail.com>
2020-06-16 13:18:53 +0200

    changed assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Utilities")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# Utilities")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Utilities")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("Lucraft Studios")]
+[assembly: AssemblyProduct("C# Utilities")]
+[assembly: AssemblyCopyright("Copyright © Lucraft Studios 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
